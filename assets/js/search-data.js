f5d2370f1ed3e00b697a54f5f3ea31378e5604b7
ChicagoPark <73331241+ChicagoPark@users.noreply.github.com>
2025-02-08 19:01:16 +0000

    Deploying to gh-pages from @ ChicagoPark/chicagopark.github.io@f5b3aa56db079d9a25bae1a649ae98bba9704748 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,10 +431,7 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-i-join-computational-imaging-group-at-washu-as-a-phd-student",
+    },{id: "news-i-join-computational-imaging-group-at-washu-as-a-phd-student",
           title: 'I join Computational Imaging Group at WashU as a PhD student.',
           description: "",
           section: "News",},{id: "projects-project-1",
