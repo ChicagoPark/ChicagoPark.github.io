72d7ccebfe5b89e11a48407c0836045fdc750fa3
ChicagoPark <73331241+ChicagoPark@users.noreply.github.com>
2025-02-17 01:36:47 +0000

    Deploying to gh-pages from @ ChicagoPark/chicagopark.github.io@d7e8ff25211776295df702911a9528a3ae2c5493 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
+  },{id: "nav-publications",
+          title: "publications",
+          description: "A complete list is available on Google Scholar",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "nav-cv",
           title: "cv",
           description: "",
           section: "Navigation",
